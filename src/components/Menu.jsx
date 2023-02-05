ad251d7d29e72a1443f156e51c11df7dc42927dc
Ainina Ndaw <58207166+ndawkunda@users.noreply.github.com>
2023-02-05 14:08:45 -0500

    mobile menu init

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,35 +3,40 @@ import { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Menu = () => {
-
-  const [menuOpen, setMenuOpen] = useState(true)
+  const [menuOpen, setMenuOpen] = useState(false)
+  const menuToggle = () => {
+    setMenuOpen(!menuOpen)
+  }
 
   return (
     <>
-      <div className='topbar'>
-        <h1 className='logo'>LOGO</h1>
-        <nav>
-          { menuOpen ? <FaBars /> : <FaTimes /> }
-  
-          <ul className='main-menu'>
-            <li className='link'>
-              <Link to='/'>Accueil</Link>
-            </li>
-            <li className='link'>
-              <Link to='/services'>Services</Link>
-            </li>
-            <li className='link'>
-              <Link to='/a-propos'>A propos</Link>
-            </li>
-            <li className='link'>
-              <Link to='/contact'>Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <nav className='header'>
+        <div className='topbar'>
+          <h1 className='logo'>LOGO</h1>
+          {menuOpen ? (
+            <FaTimes onClick={menuToggle} />
+          ) : (
+            <FaBars onClick={menuToggle} />
+          )}
+        </div>
+
+        <ul className={menuOpen ? 'main-menu menu-open' : 'main-menu'}>
+          <li className='link'>
+            <Link to='/'>Accueil</Link>
+          </li>
+          <li className='link'>
+            <Link to='/services'>Services</Link>
+          </li>
+          <li className='link'>
+            <Link to='/a-propos'>A propos</Link>
+          </li>
+          <li className='link'>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
 
 export default Menu
-
